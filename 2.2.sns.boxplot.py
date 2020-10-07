73d4f16a981bba8d6ad26b29649563dc86f3d0ee
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2020-10-07 14:36:35 +0900

    Update 2.2.sns.boxplot.py

--- 2.2.sns.boxplot.py
+++ 2.2.sns.boxplot.py
@@ -1,4 +1,5 @@
-import matplotlib.pyplot as plt
+### python3 ~.py
+
 import seaborn as sns
 import pandas as pd
 import numpy as np
@@ -9,7 +10,7 @@ df = pd.read_table('plot_input2.txt')
 x="GENE"
 hue="Tissue"
 y = "TPM"
-box_pairs = [(('IRF8','T'),('IRF8','N')),(('JUNB','T'),('JUNB','N')),(('GFI1B','T'),('GFI1B','N')),(('POU2F2','T'),('POU2F2','N')),(('MYB','T'),('MYB','N')),(('AKNA','T'),('AKNA','N')),(('IKZF1','T'),('IKZF1','N')),(('KLF2','T'),('KLF2','N')),(('IRF5','T'),('IRF5','N')),(('BTG2','T'),('BTG2','N')),(('TAL1','T'),('TAL1','N')),(('BCL11A','T'),('BCL11A','N')),(('EAF2','T'),('EAF2','N')),(('SPIB','T'),('SPIB','N')),(('KLF1','T'),('KLF1','N')),(('SPI1','T'),('SPI1','N')),(('IKZF3','T'),('IKZF3','N')),(('NFE2','T'),('NFE2','N')),(('TCF7','T'),('TCF7','N')),(('GATA1','T'),('GATA1','N'))]
+box_pairs = [(('IRF8','T'),('IRF8','N')),(('JUNB','T'),('JUNB','N'))]
 ax = sns.boxplot(data=df, x=x, y=y, hue=hue)
 ax,test_result=add_stat_annotation(ax, data=df, x=x, y=y, hue=hue, box_pairs=box_pairs,test='t-test_paired', comparisons_correction=None, loc='inside', verbose=2)
 #t-test_ind, t-test_welch, t-test_paired, Mann-Whitney, Mann-Whitney-gt, Mann-Whitney-ls, Levene, Wilcoxon, Kruskal
