a6b3e725603df0d332b5784afe5376b78eed5145
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2020-10-27 11:44:54 +0900

    Update classification_cms.py

--- classification_cms.py
+++ classification_cms.py
@@ -1,7 +1,6 @@
 import string,sys,glob
 
 CMS={}
-#GENE=['ENST00000307564_AKNA','ENST00000356842_BCL11A','ENST00000290551_BTG2','ENST00000376670_GATA1','ENST00000339463_GFI1B','ENST00000595883_SPIB','ENST00000331340_IKZF1','ENST00000357234_IRF5','ENST00000268638_IRF8','ENST00000342854_TCF7','ENST00000302754_JUNB','ENST00000294339_TAL1','ENST00000526816_POU2F2','ENST00000346872_IKZF3','ENST00000248071_KLF2','ENST00000341911_MYB','ENST00000273668_EAF2','ENST00000264834_KLF1','ENST00000435572_NFE2','ENST00000378538_SPI1']
 
 fp=open("res_prediction.txt","r")
 fp.readline()
@@ -23,7 +22,7 @@ for key,value in sorted(CMS.items()):
 		for j in value:
 			if j in line_temp1[0]:
 				for i in range(20):
-					if ('CMS1' == key) and ('PM.AS.0035.T'==j):
+					if ('CMS1' == key) and ('sample1'==j):
 						fpout[i].write("CMS"+'\t'+"TPM"+'\t'+"GENE"+'\t'+"SAMPLE"+'\n')
 						fpout[i].write(str(key)+'\t'+line_temp1[i+1]+'\t'+g[i+1]+'\t'+str(j)+'\n')
 					else:
