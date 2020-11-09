fa79b356391717217d4ac156430fa8370d41094d
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2020-11-09 13:50:05 +0900

    Update 3.ViolinPlot_sep.R

--- 3.ViolinPlot_sep.R
+++ 3.ViolinPlot_sep.R
@@ -13,7 +13,9 @@ for (i in 1:length(file_list)){
 
 	png(paste0(sample,".png"), width=800, height=600, res=200)
 	h <- ggplot(data, aes(x=Tissue, y=TPM, fill=GENE))+geom_violin(trim=FALSE)+geom_jitter(shape=16, position=position_jitter(0.2))+theme(axis.title = element_text(size=10))+theme(axis.text = element_text(face="bold",size=8))+scale_fill_hue(l=50)+coord_cartesian(ylim=c(0,50))
-	h2 <- h + stat_compare_means(method = "t.test", paired=T, label.x = 1.5, label.y = 45, label="p.signif")
+	    + stat_compare_means(method = "t.test", paired=T, label.x = 1.5, label.y = 45, label="p.signif")
 	print(h2)
+	a <- compare_means(TPM ~ CMS, method = "anova")
+	print(a)
 	dev.off()
 }
