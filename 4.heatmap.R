eb2b6ae2f677670ad39d2d7391f24c4ec115d418
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2020-11-11 13:35:04 +0900

    Update 4.heatmap.R

--- 4.heatmap.R
+++ 4.heatmap.R
@@ -1,18 +1,19 @@
 #!/usr/bin/env Rscript
 library(pheatmap)
+
 data <- read.csv("Normalized.txt",header=T,stringsAsFactors=F,sep="\t")
 
-#sub.res.ori <- data 	
 sub.res.ori <- data[,c(1:227)]
-#sub.res <- data[,2:ncol(data)]	
 sub.res <- data[,c(2:227)]
+#sub.res.ori <- data 
+#sub.res <- data[,2:ncol(data)]	
 rownames(sub.res) <- sub.res.ori[,1]
 
 #my_sample_col <- data.frame(Group = rep(c("T","N"),c(226,226)))
 #rownames(my_sample_col) <- colnames(sub.res)
 
 #colors = list(Group = c(T="red", N="blue"))
-#"dark red", "coral", "orange red", "deep sky blue", "dodger blue", "dark golden rod", "golden rod", "yellow green", "olive drab"
+##"dark red", "coral", "orange red", "deep sky blue", "dodger blue", "dark golden rod", "golden rod", "yellow green", "olive drab"
 
 png("heatmap_T.png", width=4000, height=4000, res=300)
 pheatmap(t(sub.res), cluster_rows=T, cluster_cols=T,
