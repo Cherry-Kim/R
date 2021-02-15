1eea8a8ca1d7b3f738cf7e3d1dbb516163a8bbba
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2021-02-15 13:43:32 +0900

    Update cor.r

--- cor.r
+++ cor.r
@@ -1,11 +1,6 @@
 cor <- function(){
 	dat2 <- read.delim("RAW.txt",header=T,stringsAsFactors=F)
-	str(dat2)
-	#data.frame:	48440 obs. of  9 variables:
-	# $ Feature.ID    : chr  "0610006L08Rik" "0610007P14Rik" "0610009B22Rik" "0610009E02Rik" ...
-	 #$ Raw_siNC_1.1  : num  0 36.849 23.202 0.108 0 ...
-	 #$ Raw_siNC_1    : num  0 36.505 17.436 0.247 0 ...
-
+	
 	dat2 <- dat2[,2:9]
 	res.cor <- cor(dat)
 	corrplot.mixed(res.cor, tl.cex = 0.5, number.cex=1.2, upper = "ellipse", lower = "number", tl.col = rep(c("red","blue"),times=c(8,8)))
