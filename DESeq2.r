d4fcfd1a752cc20980e242df05bfa0638f44dab2
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2021-12-27 09:45:28 -0800

    Update DESeq2.r

--- DESeq2.r
+++ DESeq2.r
@@ -1,3 +1,48 @@
+#!/usr/bin/env Rscript
+library(tximport)
+library(DESeq2)
+
+DESeq2 <- function(
+           dir
+           ){
+        samples <- read.table(file.path(dir, "samples.txt"), header = TRUE)
+
+        files_C <- paste0(  samples[which(samples$TREATMENT == "untreated"),1], ".genes.results")
+        sample.C.list <- list()
+        for( i in 1:length(files_C)){
+                sample.C.list[i] <- unlist(strsplit(files_C[i], split=".genes.results"))[1]
+        }
+
+        files_T <- paste0(  samples[which(samples$TREATMENT == "treated"),1], ".genes.results")
+        sample.T.list <- list()
+        for( i in 1:length(files_T)){
+                sample.T.list[i] <- unlist(strsplit(files_T[i], split=".genes.results"))[1]
+        }
+
+        files <- c(files_C, files_T)
+
+        sample.list <- c(sample.C.list, sample.T.list)
+        names(files) <- sample.list
+
+        txi.rsem <- tximport(files, type = "rsem", txIn = FALSE, txOut = FALSE)
+        write.table(txi.rsem$counts, "DESeq2_input.txt", col.names=NA, row.names=T, quote=F,sep='\t')
+
+        #------------------------------------------------------------------------------
+        sampleNames <- factor(as.character(sample.list))
+        sampleGroup <- factor(samples$TREATMENT)
+        sampleTable <- data.frame(sampleNames = sampleNames, sampleGroup = sampleGroup)
+
+        dds <- DESeqDataSetFromMatrix(countData = round(txi.rsem$counts), colData = sampleTable, design = ~ sampleGroup)
+        dds <- DESeq(dds)
+        dds_results <- results(dds)     #res <- results(dds, pAdjustMethod="BH")
+        write.csv(dds_results, file="DESeq2_results.csv")
+
+}
+DESeq2( dir <- "/BIO/")
+############################
+# Remove 1 column
+# cat file.txt | awk '{$1=""; print $0}' > file.out
+
 data <- read.table( "group1:group2.count.txt",  sep="\t", header=T, stringsAsFactor=F)
 
 sampleNames <- factor(as.character(unlist(strsplit(colnames(data[3:ncol(data)]), split=".fastq.gz"))))
