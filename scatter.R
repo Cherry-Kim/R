2f1c75d55ab8679f19a702f9c9058164e11090dd
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2023-03-02 14:27:36 -0800

    Create scatter.R

---
+++ scatter.R
@@ -0,0 +1,15 @@
+        data <- read.delim("input.db.t.txt",header=T,stringsAsFactors=F)
+        #  Accession Subtype Size gene1
+        #1 sample1  A            0         3
+        
+        library(dplyr)
+        library(ggplot2)
+        A <- subset(data, Subtype=="A") 
+        ggplot(A, aes(x=gene1, y=Size)) + geom_point()+ geom_smooth(method=lm, se=FALSE)
+            + annotate("text", x=400,y=6, label= "A", col="blue", size=14)
+            + ggtitle("A") + theme(plot.title = element_text(size = 36,face = "bold"))
+            + xlab("gene1")+ theme(axis.text = element_text(size = 19,face = "bold"))
+            + theme(axis.title.y = element_text(size =33,face = "bold"))
+            + theme(axis.title.x = element_text(size = 38,face = "bold"))
+        cor.test(A$Size, A$gene1, method="spearman")
+
