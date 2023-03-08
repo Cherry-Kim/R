e1c3802231d3124626e53f31f1f757dca6f7f696
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2023-03-08 12:12:45 -0800

    Update barplot.R

--- barplot.R
+++ barplot.R
@@ -18,4 +18,15 @@ Bar <- function(){
             +theme(panel.background = element_rect(fill = "white"), axis.line.y.left = element_line(color = "black"))
             + ylim(0,1)+xlab("")+ylab("value")
             +geom_text(aes(label = round(cor,2)),face = "bold",size = 7)
+        
+        B <- data[1:5,5:8] #negative value
+        df <- B%>%arrange(desc(B_cor))
+        df$Symbol <- factor(rev(B$B_Symbol), levels = rev(B$B_Symbol))
+        ggplot(df, aes(x =Symbol, y =B_cor), width = 0.6) +geom_col(fill = "gray70") +ggtitle("LumA")
+             +coord_flip()+theme(text = element_text(size = 30))+ theme(axis.text = element_text(size = 30,face = "bold")) 
+             + theme(plot.title = element_text(size = 36,face = "bold",colour="purple")) 
+             +theme(panel.background = element_rect(fill = "white"), axis.line.y.left = element_line(color = "black"))
+             + ylim(-1,0)
+             +xlab("")+ylab("Correlation Coefficient")+geom_text(aes(label = round(LumA_cor,2)),face = "bold",size = 12)
+        
 }
