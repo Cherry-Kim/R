5c960582e038380b0504e66bcf6ca984e4263ee7
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2023-07-06 12:07:20 -0700

    Update scatter.R

--- scatter.R
+++ scatter.R
@@ -13,3 +13,17 @@
             + theme(axis.title.x = element_text(size = 38,face = "bold"))
         cor.test(A$Size, A$gene1, method="spearman")
 
+
+#-----------------------------------------------------
+        data1 <- read.delim('temp1.txt', header=T)
+        data1$group <- "temp1"     
+        data2 <- read.delim('temp2.txt', header=T)
+        data2$group <- "temp2" 
+
+        merged_data <- rbind(data1, data2)
+        ggplot(merged_data, aes(x = x, y = y, color = group, shape = group)) +
+          geom_point(size = 2) +
+          labs(x = "X", y = "Y", color = "group") +
+          ggtitle("test") + theme(plot.title = element_text(size = 36,face = "bold",hjust = 0.5, color="purple")) +          theme(axis.text = element_text(size = 20), legend.text = element_text(size = 20, face = "bold")) +
+          theme(axis.title.x = element_text(size = 38,face = "bold")) +   
+          theme(axis.title.y = element_text(size =33,face = "bold"))   
